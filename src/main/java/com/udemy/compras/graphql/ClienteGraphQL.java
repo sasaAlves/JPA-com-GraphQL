5efc35270f86993056cc0376933aaf871946b7f9
sasaAlves <31769107+sasaAlves@users.noreply.github.com>
2022-11-07 09:51:36 -0300

    JPA com GrapghQL

---
+++ src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
@@ -0,0 +1,36 @@
+package com.udemy.compras.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    public Cliente cliente(Long id) {
+        return clienteService.findById(id);
+    }
+
+    public List<Cliente> clientes() {
+        return clienteService.findAll();
+    }
+
+    public Cliente saveCliente(Long id, String nome, String email) {
+        Cliente c = new Cliente();
+        c.setId(id);
+        c.setNome(nome);
+        c.setEmail(email);
+
+        return clienteService.save(c);
+    }
+
+    public Boolean deleteCliente(Long id) {
+         return clienteService.deleteById(id);
+    }
+}
